09b13250858eb6b3297fd293b52b7cfe4a6739e5
Calvin-Midgley <146990364+Calvin-Midgley@users.noreply.github.com>
2024-09-04 13:26:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 window.onload = function() {
   let params = new URLSearchParams(document.location.search);
   let title = params.get("title");
-  let date = params.get("date");
+  let date = Date(params.get("date"));
   document.getElementById("title").textContent = title;
-  document.getElementById("timer").textContent = date;
+  setInterval(function() {
+    let currentTime = Date();
+    let difference = date-currentTime;
+    document.getElementById("timer").textContent = difference;
+  }, 1000);
 };
