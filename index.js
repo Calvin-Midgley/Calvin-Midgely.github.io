c095a2129e0d457ec3fdd8b48e3d5b3db0080d34
Calvin-Midgley <146990364+Calvin-Midgley@users.noreply.github.com>
2024-09-04 13:29:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ window.onload = function() {
   setInterval(function() {
     let currentTime = Date();
     let difference = date-currentTime;
-    document.getElementById("timer").textContent = currentTime;
+    document.getElementById("timer").textContent = "Current Time: " + currentTime + "\nEnd Time: " + date + "\nDifference: " + difference;
   }, 1000);
 };
