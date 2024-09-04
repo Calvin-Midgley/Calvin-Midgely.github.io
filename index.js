ba3581c4763e59db9d9c2fb6379f261f57f03df1
Calvin-Midgley <146990364+Calvin-Midgley@users.noreply.github.com>
2024-09-04 12:56:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ window.onload = function() {
   let url_full = document.url;
   const url_split = url_full.split("?");
   let url_params = url_split[1];
+  document.getElementById("url").textContent = url_full;
   document.getElementById("timer").textContent = url_params;
 };
