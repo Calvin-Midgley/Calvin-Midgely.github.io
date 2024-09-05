b49f56dd7fac487a451c92f6ad5061a7da1745cb
Calvin-Midgley <146990364+Calvin-Midgley@users.noreply.github.com>
2024-09-04 21:54:39 -0400

    improved unit seperation + say 'time past' when selected date is before current date

--- index.js
+++ index.js
@@ -3,29 +3,42 @@ window.onload = function() {
   let title = params.get("title");
   let date = new Date(params.get("date"));
   document.getElementById("title").textContent = title;
-  document.getElementById("date").textContent = "Time Until " + date + ":";
   setInterval(function() {
-    let diff = Math.abs(Date.now()-date.getTime());
+    let diff = Date.now()-date.getTime();
+    if (diff < 0) {
+      document.getElementById("date").textContent = "Time Until " + date + ":";
+    } else {
+      document.getElementById("date").textContent = "Time Since " + date + ":";
+    }
+
+    diff = Math.abs(diff);
     let ms = Math.floor(diff%1000);
     let s = Math.floor((diff/1000)%60);
     let m = Math.floor((diff/60000)%60);
     let h = Math.floor((diff/3600000)%24);
     let d = Math.floor((diff/86400000)%365);
     let y = Math.floor(diff/31536000000);
+    
+    let showy = y > 0;
+    let showd = d > 0 || showy;
+    let showh = h > 0 || showd;
+    let showm = m > 0 || showh;
+    let shows = s > 0 || showm;
+    
     let output = ""
-    if (y > 0) {
+    if (showy) {
       output = output + y + " years, ";
     }
-    if (d > 0) {
+    if (showd) {
       output = output + d + " days, ";
     }
-    if (h > 0) {
+    if (showh) {
       output = output + h + " hours, ";
     }
-    if (m > 0) {
+    if (showm) {
       output = output + m.toString().padStart(2,'0') + " minutes, ";
     }
-    if (s > 0) {
+    if (shows) {
       output = output + s.toString().padStart(2,'0') + " seconds, ";
     }
     output = output + ms.toString().padStart(4,'0') + "milliseconds";
