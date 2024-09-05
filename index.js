c631f409d5c1e5e0c637fcb426ff89daffc0e153
Calvin-Midgley <146990364+Calvin-Midgley@users.noreply.github.com>
2024-09-04 20:45:25 -0400

    fix NaN in time difference calculation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ window.onload = function() {
   document.getElementById("title").textContent = title;
   setInterval(function() {
     let currentTime = Date();
-    let difference = Math.abs(currentTime-date);
+    let difference = Math.abs(currentTime.getTime()-date.getTime());
     document.getElementById("timer").textContent = "Current Time: " + currentTime + "\nEnd Time: " + date + "\nDifference: " + difference;
   }, 1000);
 };
